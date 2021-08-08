3a20fc1bc726ec47781f86143d14ceb75feed356
Jose David <mq.josedavid@gmail.com>
2021-08-08 17:13:59 -0400

    Try to manager this fuctionality

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@material-ui/core/styles";
+
+// Create a theme instance.
+const theme = createTheme({
+    palette:{
+        type:"dark"
+    }
+});
+
+export default theme;
